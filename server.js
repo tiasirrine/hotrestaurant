f32ba0f18fa27279f44c4ff73e0d7bbf47faf4e2
Tia Sirrine <35241060+tiasirrine@users.noreply.github.com>
2018-04-23 20:14:09 -0700

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.text());
 app.use(bodyParser.json());
 
 // Basic route that sends the user first to the AJAX Page
@@ -34,53 +33,3 @@ var tables = [
   email: "",
   uniqueID: ""},
   ];
-
-app.get('/api/tables', function (req, res) {
-  // console.log('table data requested');
-  // var response = "testing";
-  res.json(tables);
-});
-
-// reserve API call
-app.post('/api/reserve', function (req, res) {
-	console.log('reserve request submitted');
-	console.log(req.body);
-
-  var newReservation = req.body;
-
-  tables.push(newReservation);
-
-   //console.log(tables);
-
-  // Check if user is in the first 5 in list
-  var isBooked;
-  if(tables.length <= 5){
-    isBooked = true;
-  }
-  else{
-    isBooked = false;
-  }
-
-  res.json(isBooked);
-
-});
-
-app.post('/api/clear', function (req, res) {
-  console.log('clear all tables');
-  tables = [];
-  res.sendFile(path.join(__dirname, 'tables.html'));
-});
-
-app.post('/api/killreservation', function (req, res) {
-  console.log(req.body.id);
-
-  tables.splice(req.body.id, 1);
-  // console.log(tables);
-  res.json(tables);
-});
-
-// Starts the server to begin listening
-// =============================================================
-app.listen(PORT, function() {
-  console.log("App listening on PORT " + PORT);
-});
